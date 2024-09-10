b0835e0783fec5a45ae34d11e1494528a546643c
aecheverri <alejandrodecheverri@gmail.com>
2024-09-10 17:48:58 -0300

    andaaaa

--- src/MapaEstatico.java
+++ src/MapaEstatico.java
@@ -5,10 +5,11 @@ public class MapaEstatico<K, V> implements Mapa<K,V> {
     private Integer cantElems;
     
     MapaEstatico (){
-       claves = new K[LONG_DEFAULT];
-       significados = new V[LONG_DEFAULT];
+       claves = (K[]) new Object[LONG_DEFAULT];
+       significados = (V[]) new Object[LONG_DEFAULT];
        cantElems = 0;  
     }
+
     @Override
     public void agregar(K clave, V valor){
         
